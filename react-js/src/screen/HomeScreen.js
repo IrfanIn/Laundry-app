3e372c7f3f6a8f61645a1ef003e1a71c0da2af28
irfan rifani <feeinitfan@gmail.com>
2023-01-06 06:10:20 +0700

    Landing page

---
+++ react-js/src/screen/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../util/Header'
+
+const HomeScreen = () => {
+    return (
+        <>
+            <Header />
+        </>
+    )
+}
+
+export default HomeScreen
---
+++ react-js/src/util/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <h3>Laundrian</h3>
+            <ul>
+                <li><a href="/paket">Paket cuci</a></li>
+                <li><a href="/tentang">Tentang kami</a></li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header
